47e3e23312bafff384512e2afc04d42238bbc8e9
Asjal Zaka <asjalzaka16@gmail.com>
2023-02-17 16:16:08 +0500

    Commit Before Starting Javascript

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello World!!')
+// const dataInput = document.getElementById('input')
+const content = 'Hello Ppl'
+const url = `http://api.qrserver.com/v1/create-qr-code/?data=${content}`;
+const qrImg = document.getElementById('qrcode')
+const btn = document.getElementById('btn')
+// const link = `http(s)://api.qrserver.com/v1/read-qr-code/?fileurl=${encodeURIComponent(url)}`;
+// function read() {
+//     fetch(link)
+//         .then((response) => response.json())
+//         .then((data) => console.log(data[0].symbol[0].data));
+// }
+
+btn.onclick = (event) => {
+    event.preventDefault()
+    console.log('REQ MADE')
+    fetch(url, { mode: "no-cors" })
+        .then((response) => console.log(response.json()))
+    qrImg.setAttribute('src', url)
+}
